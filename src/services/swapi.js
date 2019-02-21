85f0a39f63d62916fedcc2c3ed6729c15a88b57b
XoldpandaX <denis.d.shakin@gmail.com>
2019-02-21 21:17:28 +0700

    trasfered service && added base components files

--- src/services/swapi.js
+++ src/services/swapi.js
@@ -1,3 +1,39 @@
-class SwapiService {
-
+export default class SwapiService {
+  _apiBaseUrl = 'https://swapi.co/api/';
+  
+  async getResource(url) {
+    const response = await fetch(`${ this._apiBaseUrl }${ url }`);
+    
+    if (!response.ok) {
+      throw new Error(`could not fetch ${ url }, recieved ${ response.status }`);
+    }
+    
+    const res = await response.json();
+    
+    return res;
+  }
+  
+  getAllPeople() {
+    return this.getResource('people');
+  }
+  
+  getPerson(id) {
+    return this.getResource(`people/${ id }`);
+  }
+  
+  getAllPlanets() {
+    return this.getResource('planets');
+  }
+  
+  getPlanet(id) {
+    return this.getResource(`planet/${ id }`);
+  }
+  
+  getAllStarships() {
+    return this.getResource('starships');
+  }
+  
+  getStarship(id) {
+    return this.getResource(`starship/${ id }`);
+  }
 }
