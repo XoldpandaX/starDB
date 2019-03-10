d9581f5303cefa49b967ed8f32697b85e8b47aae
XoldpandaX <denis.d.shakin@gmail.com>
2019-03-10 19:33:30 +0700

    created very simple auth

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,9 +1,13 @@
+import LoginPage from './login-page';
 import PeoplePage from './people-page';
 import PlanetsPage from './planets-page';
+import SecretPage from './secret-page';
 import StarShipPage from './starships-page';
 
 export {
+  LoginPage,
   PeoplePage,
   PlanetsPage,
+  SecretPage,
   StarShipPage
 }
